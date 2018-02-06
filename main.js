68356ddedddb6fe316459d1133f7b88b00bb9993
Steven Mao <31294078+nzrugby@users.noreply.github.com>
2018-02-06 20:33:46 +1300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
-//import Greeter from './Greeter';
 import Shopping1 from './src/Shopping'
 import './General.css'
 import {Provider,connect} from 'react-redux'
 import {createStore} from 'redux'
 import {rootReducer} from './src/Reducer/reducer'
-//import 'bootstrap/css/bootstrap.css'
 
 let store = createStore(reducer)
 const ShopCart = connect(mapStateToProps,mapDispatchToProps)(Shopping1)
