0c9474983951e2b8ebd94eb0374b77e5522e816c
Steven Mao <31294078+nzrugby@users.noreply.github.com>
2018-02-09 22:44:12 +1300

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,11 @@
 import React,{Component} from 'react'
+import {connect} from 'react-redux'
+import { mapDispatchToPropsProductsContainer } from './Reducer/reducer';
 
 class Search extends Component{
 
-    handleClick(){
-        var search = document.getElementById('search').value;
-        this.props.searchItem(search);
-    }
-
     render(){
+        const {searchClick} = this.props;
         return(
             <div className='row'>
                 <div className='col-md-8 col-md-offset-2'>
@@ -16,8 +14,9 @@ class Search extends Component{
                             <input className="form-control"  id="search" type="text" placeholder="Please input here"/>
                         </div>
                         <div className='col-md-3'>
-                            <button className="btn" onClick={this.handleClick.bind(this)}>Search</button>
+                            <button className="btn" onClick={searchClick}>Search</button>
                         </div>
+                        
                     </div>
                 </div>
             </div>
@@ -25,4 +24,5 @@ class Search extends Component{
     }
 }
 
-export default Search
+const Search1 = connect(null,mapDispatchToPropsProductsContainer)(Search)
+export default Search1
